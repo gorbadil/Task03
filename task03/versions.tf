9e50fe2282b6508a729abebc8d1aed7366b13d6e
Ersin Mutlu <gorbadil@192.168.1.4>
2024-08-20 03:14:41 +0300

    verification 3

--- task03/versions.tf
+++ task03/versions.tf
@@ -2,8 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.110.0"
+      version = ">=3.110.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+
   }
 
   required_version = ">= 1.5.7"

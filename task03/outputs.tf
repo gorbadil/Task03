a9026bc54786d8a007e0e030683602738b1686c4
Ersin Mutlu <gorbadil@192.168.1.4>
2024-08-20 03:11:08 +0300

    verification 2

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,11 +1,11 @@
-output "Resource Group ID" {
-  value = azurerm_resource_group.rg.id
+output "rg_id" {
+  value = "Resource Group ID" + azurerm_resource_group.rg.id
 }
 
-output "Storage account blob service primary endpoint" {
-  value = azurerm_storage_account.sa.primary_blob_endpoint
+output "sa_blob_endpoint" {
+  value = "Storage account blob service primary endpoint" + azurerm_storage_account.sa.primary_blob_endpoint
 }
 
-output "Virtual network ID" {
-  value = azurerm_virtual_network.vnet.id
+output "nvnet_id" {
+  value = "Virtual network ID" + azurerm_virtual_network.vnet.id
 }

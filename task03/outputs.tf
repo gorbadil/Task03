dab68a9bf448144d3ad0a6985b5468dca740b0f6
Ersin Mutlu <gorbadil@192.168.1.4>
2024-08-20 03:24:00 +0300

    verification 4

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,14 +1,14 @@
 output "rg_id" {
-  value       = "Resource Group ID" + azurerm_resource_group.rg.id
+  value       = "Resource Group ID ${azurerm_resource_group.rg.id}"
   description = "value of the id attribute of the azurerm_resource_group resource"
 }
 
 output "sa_blob_endpoint" {
-  value       = "Storage account blob service primary endpoint" + azurerm_storage_account.sa.primary_blob_endpoint
+  value       = "Storage account blob service primary endpoint ${azurerm_storage_account.sa.primary_blob_endpoint}"
   description = "value of the primary_blob_endpoint attribute of the azurerm_storage_account resource"
 }
 
 output "vnet_id" {
-  value       = "Virtual network ID" + azurerm_virtual_network.vnet.id
+  value       = "Virtual network ID ${azurerm_virtual_network.vnet.id}"
   description = "value of the id attribute of the azurerm_virtual_network resource"
 }
--- task03/versions.tf
+++ task03/versions.tf
@@ -1,17 +1,11 @@
 terraform {
+  required_version = ">= 1.5.7"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.110.0"
+      version = ">=3.110.0, <4.0.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-
-  }
-
-  required_version = ">= 1.5.7"
 }
 
 provider "azurerm" {

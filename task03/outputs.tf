56c2286f40443187f0ced6918e181c8c73d0aa99
Ersin Mutlu <gorbadil@192.168.1.4>
2024-08-20 03:57:47 +0300

    verification 7

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,14 +1,14 @@
 output "rg_id" {
-  value       = "Resource Group ID ${azurerm_resource_group.rg.id}"
-  description = "value of the id attribute of the azurerm_resource_group resource"
+  value       = azurerm_resource_group.rg.id
+  description = "Resource Group ID"
 }
 
 output "sa_blob_endpoint" {
-  value       = "Storage account blob service primary endpoint ${azurerm_storage_account.sa.primary_blob_endpoint}"
-  description = "value of the primary_blob_endpoint attribute of the azurerm_storage_account resource"
+  value       = azurerm_storage_account.sa.primary_blob_endpoint
+  description = "Storage account blob service primary endpoint"
 }
 
 output "vnet_id" {
-  value       = "Virtual network ID ${azurerm_virtual_network.vnet.id}"
-  description = "value of the id attribute of the azurerm_virtual_network resource"
+  value       = azurerm_virtual_network.vnet.id
+  description = "Virtual network ID"
 }
